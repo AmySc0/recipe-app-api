d7c95d2f05836e4c1940f78cecb892d462117957
Amy Scott <amy@fuzzydigital.com>
2020-06-03 14:48:34 +0100

    Added recipe model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -62,3 +62,14 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """Test the recipe string representation"""
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='Steak & Mushroom Sauce',
+            time_mins=5,
+            price=5.00
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
