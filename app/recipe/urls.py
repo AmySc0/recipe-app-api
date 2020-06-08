5d294230487648d069479d2ed528216ef9c1b66c
Amy Scott <amy@fuzzydigital.com>
2020-06-08 11:56:04 +0100

    Added recipe image upload feature

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -1,3 +1,5 @@
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
@@ -13,4 +15,4 @@ app_name = 'recipe'
 
 urlpatterns = [
     path('', include(router.urls))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
